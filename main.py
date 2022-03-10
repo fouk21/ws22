aa8a2cb6512d1ab3b15dce065dbce78640ae7ef4
Christos Foukanelis <75619646+fouk21@users.noreply.github.com>
2022-03-11 00:36:33 +0200

    guessing game

--- main.py
+++ main.py
@@ -1,6 +1,26 @@
+import random
+import subprocess
 
+def commands():
+    return True
 
-
+def guessingGame():
+    print("GUESSING GAME: GUESS THE NUMBER")
+    number  = random.randint(1,10)
+    print(number)
+    tries = 3
+    win = False
+    while tries > 0:
+        guess = int(input("Guess what number i am thinking: "))
+        if number == guess:
+            print("Congratulations!")
+            win = True
+            break
+        tries -= 1
+        print("Sorry wrong guess, " + str(tries) + " left")
+    if not win:
+        print("You lost")
 
 if __name__ == '__main__':
-    print("hello world")
+    guessingGame()
+    #print("hello world")
